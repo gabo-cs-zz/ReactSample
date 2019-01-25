e4dab6325c42740f18e30bcb34d951cfeef6d2b8
Gabriel Coronado <gabrielomar2809@gmail.com>
2019-01-25 01:36:09 -0500

    Getting IDs IDs got skipping duplicated ones

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 
 import Album from './Album'
 
+const dropDown = {
+  display: 'block'
+}
+
 class Layout extends Component {
   constructor(){
     super();
@@ -19,10 +23,24 @@ class Layout extends Component {
         })
       );
   }
+
+  getIds(){
+    let ids = [];
+    ids.push(this.state.albums[0].userId);
+    for(let i = 1; i < this.state.albums.length; i++){
+      if(this.state.albums[i].userId !== ids[ids.length-1]){
+        ids.push(this.state.albums[i].userId);
+      }
+    }
+    return ids;
+  }
  
   render(){
     return(
       <div>
+        <select style={dropDown}>
+          { this.state.albums.length > 0 && this.getIds().map(item => <option key={item}> {item} </option> ) }
+        </select>
         { this.state.albums.map(item => <Album key={item.id} {...item} /> )}
       </div>
     );
